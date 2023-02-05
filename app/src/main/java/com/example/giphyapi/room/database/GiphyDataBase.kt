ae2cac98fbe2ce4a4572c8b87692ab028c0a6bda
DimaMerzlov <dimamerzlov1@example.com>
2023-02-05 10:27:25 +0200

    room increase version

--- app/src/main/java/com/example/giphyapi/room/database/GiphyDataBase.kt
+++ app/src/main/java/com/example/giphyapi/room/database/GiphyDataBase.kt
@@ -2,10 +2,11 @@ package com.example.giphyapi.room.database
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
-import com.example.giphyapi.room.GiphyEntity
+import com.example.giphyapi.room.entity.GiphyEntity
 import com.example.giphyapi.room.dao.GiphyDao
+import com.example.giphyapi.room.entity.GiphyEntityDelete
 
-@Database(entities = [GiphyEntity::class], version = 1)
-abstract class GiphyDataBase: RoomDatabase() {
+@Database(entities = [GiphyEntity::class, GiphyEntityDelete::class], version = 2)
+abstract class GiphyDataBase : RoomDatabase() {
     abstract fun giphyDao(): GiphyDao
 }
